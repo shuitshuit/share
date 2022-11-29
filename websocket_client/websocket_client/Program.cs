c590df870df4a83b5f51016fdc9a08eff2eaa648
shuit <shuitemail@gmail.com>
2022-11-29 11:05:39 +0900

    shuit

--- websocket_client/websocket_client/Program.cs
+++ websocket_client/websocket_client/Program.cs
@@ -1,43 +1,11 @@
-﻿using Fleck;
-using Npgsql;
-
+﻿
 namespace ClientServer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var server = new WebSocketServer("ws://0.0.0.0:8000");
 
-
-            server.Start(socket =>
-            {
-                socket.OnOpen = () => OnOpen(socket);
-                socket.OnClose = () => OnClose(socket);
-                socket.OnMessage = message => OnMessage(socket, message);
-                socket.OnBinary = bytes => OnBinary(socket, bytes);
-            });
-
-            string? data = "";
-            while (true)
-            {
-                data = Console.ReadLine();
-                if (data == null)
-                {
-                    continue;
-                }
-                else if (data.StartsWith("exit"))
-                {
-                    SendMessageToAll(data);
-                    break;
-                }
-                else
-                {
-                    SendMessageToAll(data);
-                }
-            }
-
-            server.Dispose();
         }
     }
 }
