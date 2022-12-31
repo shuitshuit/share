31c80c88f56bd8c829bd9e10d985b71fa2e2651c
shuit <shuitemail@gmail.com>
2023-01-01 00:45:44 +0900

    shuit

---
+++ web/web/Data/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+
+namespace web.Data
+{
+    public class User : PageModel
+    {
+        private readonly string ID;
+        private readonly string __Name;
+        private readonly string __Email;
+        public string Name { get => __Name; }
+        public string Email { get => __Email; }
+
+
+        public User()
+        {
+            var context = new HttpContextAccessor().HttpContext;
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            var client = context.Request;
+            Console.WriteLine("w/"+client.Cookies["ambb"]);
+        }
+    }
+}
--- web/web/Program.cs
+++ web/web/Program.cs
@@ -1,7 +1,8 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using NLog;
+using NLog.Config;
+using NLog.Targets;
 using web.Data;
+using LogLevel = NLog.LogLevel;
 
 namespace web
 {
@@ -9,17 +10,34 @@ namespace web
     {
         public static void Main(string[] args)
         {
+            var config = new LoggingConfiguration();
+
+            // FileTargetを生成し LoggingConfigurationに設定
+            var fileTarget = new FileTarget();
+            config.AddTarget("file", fileTarget);
+
+            // fileTargetのプロパティを設定
+            fileTarget.Name = "f";
+            fileTarget.FileName = "C:\\Users\\shuitt\\ドキュメント\\programing\\share\\web\\web\\Logs\\${shortdate}.log";
+            fileTarget.Layout = "${longdate} [${uppercase:${level}}] ${message}";
+
+            // LoggingRuleを定義
+            var rule1 = new LoggingRule("*", LogLevel.Debug, fileTarget);
+            config.LoggingRules.Add(rule1);
+
+            // 設定を有効化
+            LogManager.Configuration = config;
             Logger log = LogManager.GetCurrentClassLogger();
-            log.Info("program is start");
+            log.Debug("program is start");
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddHttpContextAccessor();
 
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
